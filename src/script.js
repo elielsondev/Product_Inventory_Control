468ae89add51e3b3af83e7b37c648c624fd4ccae
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-09-02 01:33:17 -0300

    feat - capturing product information and inserting it into the matrix

--- src/script.js
+++ src/script.js
@@ -7,12 +7,36 @@ console.log("Script loaded successfully.");
 // Requisitos:
 
 // • Cada produto deve ter: nome, preço e quantidade em estoque.
+let productName = prompt("Digite o nome do produto:");
+let productPrice = Number(prompt("Digite o preço do produto:")).toFixed(2).replace('.', ',');
+let productQuantity = Number(prompt("Digite a quantidade em estoque:"));
+
 
 // • Usar uma matriz para armazenar os produtos.
+const products = [];
+
+function addProduct(name, price, quantity) {
+    // • Cadastrar novo produto (não pode duplicar nomes).
+    // Verifica se o produto já existe
+    const existingProduct = products.find(product => product.name === name);
+    
+    if (existingProduct) {
+        console.log("Produto já existe. Não é possível duplicar nomes.");
+    }
+    
+    products.push([ name, price, quantity ]);
+
+    console.log(`
+        ${name}, R$${price}, ${quantity}unidades. 
+        Adicionado ao estoque.
+    `);
+    
+};
+
+addProduct(productName, productPrice, productQuantity);
 
 // • Funcionalidades obrigatórias (menu interativo com prompt):
 
-// • Cadastrar novo produto (não pode duplicar nomes).
 
 // • Listar todos os produtos no console.
 
